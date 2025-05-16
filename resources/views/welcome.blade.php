85cc04d633cac6085b37c30b71e12c93d5b3cf3f
José Vitor <Josevitornascimentolopes@gmail.com>
2025-05-16 08:06:42 -0300

    feat: adicionado front-end do header

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Supermercado Moderno</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="{{asset('css/style.min.css')}}">
 </head>
 <body>
 
-    <h1>Olá, mundo!</h1>
-    
+    @include('sessoes-min')
+
+    <script src="{{asset('js/app.min.js')}}"></script>
 </body>
 </html>
